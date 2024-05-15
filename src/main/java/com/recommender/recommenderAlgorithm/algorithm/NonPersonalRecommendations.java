2af579dcf89b55468fa3c8e1879b3f046dca31d1
katerina <katerina_panina1@mail.ru>
2024-05-15 23:36:15 +0300

    fix serials name output and sorting the results

--- src/main/java/com/recommender/recommenderAlgorithm/algorithm/NonPersonalRecommendations.java
+++ src/main/java/com/recommender/recommenderAlgorithm/algorithm/NonPersonalRecommendations.java
@@ -3,23 +3,25 @@ package com.recommender.recommenderAlgorithm.algorithm;
 import com.recommender.recommenderAlgorithm.models.Log;
 import com.recommender.recommenderAlgorithm.repositories.LogRepository;
 import com.recommender.recommenderAlgorithm.services.LogService;
+import com.recommender.recommenderAlgorithm.services.SerialService;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping(path = "api/nonPersonal")
 public class NonPersonalRecommendations {
     private final LogService logService;
+    private final SerialService serialService;
     private HashMap<Integer, Integer> mapViews;
     @Autowired
-    public NonPersonalRecommendations(LogService logService) {
+    public NonPersonalRecommendations(LogService logService, SerialService serialService) {
         this.logService = logService;
+        this.serialService = serialService;
         mapViews = new HashMap<>();
     }
     public void calculateWatches(){
@@ -34,12 +36,21 @@ public class NonPersonalRecommendations {
             }
         }
     }
-//    public List<String> formatMap(){
-//
-//    }
+    public List<String> formatMap() {
+        List<String> result = new LinkedList<String>();
+        Map<Integer, Integer> sortedMap = new TreeMap<Integer, Integer>(
+                Comparator.comparing(mapViews::get, (s1, s2) -> {
+                    return s2.compareTo(s1);}));
+        sortedMap.putAll(mapViews);
+        Set<Integer> keys = sortedMap.keySet();
+        for (Integer key:keys) {
+            result.add(serialService.getById(key.longValue()).getSerialName());
+        }
+        return result;
+    }
     @GetMapping
-    public String getMostWatched(){
+    public List<String> getMostWatched(){
         calculateWatches();
-        return mapViews.toString();
+        return formatMap();
     }
 }
