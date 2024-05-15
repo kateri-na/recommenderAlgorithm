8fab16ff62f516e2f67791f89ccd2b11faed2a9f
katerina <katerina_panina1@mail.ru>
2024-05-15 22:48:10 +0300

    fix missing annotation

--- src/main/java/com/recommender/recommenderAlgorithm/repositories/LogRepository.java
+++ src/main/java/com/recommender/recommenderAlgorithm/repositories/LogRepository.java
@@ -2,6 +2,10 @@ package com.recommender.recommenderAlgorithm.repositories;
 
 import com.recommender.recommenderAlgorithm.models.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
+    List<Log> findAllByEventEquals(String event);
 }
