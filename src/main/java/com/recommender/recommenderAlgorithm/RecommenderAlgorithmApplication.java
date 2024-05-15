9ba4a5e59927309fd07b038dca3e4c42aa6e4bb0
katerina <katerina_panina1@mail.ru>
2024-05-15 22:51:23 +0300

    fix redundant command

--- src/main/java/com/recommender/recommenderAlgorithm/RecommenderAlgorithmApplication.java
+++ src/main/java/com/recommender/recommenderAlgorithm/RecommenderAlgorithmApplication.java
@@ -1,5 +1,6 @@
 package com.recommender.recommenderAlgorithm;
 
+import com.recommender.recommenderAlgorithm.algorithm.NonPersonalRecommendations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
