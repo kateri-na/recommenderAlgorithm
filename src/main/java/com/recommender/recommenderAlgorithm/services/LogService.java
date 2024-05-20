c85cc50f200f22206b03f1873997b5efc5c8292a
katerina <katerina_panina1@mail.ru>
2024-05-21 01:11:31 +0300

    fix methods for distinct user`s ids and getting logs by ids

--- src/main/java/com/recommender/recommenderAlgorithm/services/LogService.java
+++ src/main/java/com/recommender/recommenderAlgorithm/services/LogService.java
@@ -5,6 +5,9 @@ import com.recommender.recommenderAlgorithm.repositories.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -20,4 +23,11 @@ public class LogService {
     public List<Log> getWatchedLogs(){
         return logRepository.findAllByEventEquals("просмотр серии");
     }
+    public List<Log> getUsersLogsById(Long id){
+        return logRepository.findAllByUserId(id.longValue());
+    }
+    public List<Integer> getDistinctUsersIds(){
+        return logRepository.findDistinctByUserId();
+    }
+
 }
