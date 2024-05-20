87462b203c35a3f55ba93bd8120afb3896245cac
katerina <katerina_panina1@mail.ru>
2024-05-21 01:10:40 +0300

    fix query method

--- src/main/java/com/recommender/recommenderAlgorithm/repositories/LogRepository.java
+++ src/main/java/com/recommender/recommenderAlgorithm/repositories/LogRepository.java
@@ -2,10 +2,14 @@ package com.recommender.recommenderAlgorithm.repositories;
 
 import com.recommender.recommenderAlgorithm.models.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
     List<Log> findAllByEventEquals(String event);
+    List<Log> findAllByUserId(Long id);
+    @Query("select distinct l.userId from Log l")
+    List<Integer> findDistinctByUserId();
 }
