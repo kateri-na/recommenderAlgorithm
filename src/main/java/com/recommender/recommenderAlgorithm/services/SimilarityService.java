86abca6d1a83514dcddcce68c42662d61b6a2e10
katerina <katerina_panina1@mail.ru>
2024-05-22 01:27:40 +0300

    add similarity service for adding similarity to database

---
+++ src/main/java/com/recommender/recommenderAlgorithm/services/SimilarityService.java
@@ -0,0 +1,24 @@
+package com.recommender.recommenderAlgorithm.services;
+
+import com.recommender.recommenderAlgorithm.models.Similarities;
+import com.recommender.recommenderAlgorithm.repositories.SimilarityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SimilarityService {
+    private final SimilarityRepository similarityRepository;
+    @Autowired
+    public SimilarityService(SimilarityRepository similarityRepository) {
+        this.similarityRepository = similarityRepository;
+    }
+    public List<Similarities> getSimilarities(){
+        return similarityRepository.findAll();
+    }
+    public void addSimilarity(Integer serialRowId, Integer serialColumnId, Double similarity){
+        Similarities similarities = new Similarities(serialRowId, serialColumnId, similarity);
+        similarityRepository.save(similarities);
+    }
+}
