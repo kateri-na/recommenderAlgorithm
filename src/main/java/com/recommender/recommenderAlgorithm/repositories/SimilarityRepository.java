69bdedda47795df74025da83204ccb6a3a1088a9
katerina <katerina_panina1@mail.ru>
2024-05-22 17:20:06 +0300

    fix method for getting the neighborhood

--- src/main/java/com/recommender/recommenderAlgorithm/repositories/SimilarityRepository.java
+++ src/main/java/com/recommender/recommenderAlgorithm/repositories/SimilarityRepository.java
@@ -8,8 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SimilarityRepository extends JpaRepository<Similarities, Long> {
-    @Query(value = "select s from Similarities s where s.serialColumnId = :serialId or s.serialRowId = :serialId " +
-            "and s.similarity > :constraint")
+    @Query(value = "select s from Similarities s where (s.serialColumnId = :serialId or s.serialRowId = :serialId) and s.similarity > :constraint")
     List<Similarities> findAllGreaterThanConstraint(@Param("serialId") Long serialId,
                                         @Param("constraint") double similarityConstraint);
 }
