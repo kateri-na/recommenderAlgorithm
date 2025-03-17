5c6e5385d1a1ec1beb153024e09ef39bd67f6451
katerina <katerina_panina1@mail.ru>
2025-03-17 22:01:25 +0300

    fix controller from rest to web

--- src/main/java/com/recommender/recommenderAlgorithm/controllers/NonPersonalRecommendations.java
+++ src/main/java/com/recommender/recommenderAlgorithm/controllers/NonPersonalRecommendations.java
@@ -1,18 +1,22 @@
 package com.recommender.recommenderAlgorithm.controllers;
 
 import com.recommender.recommenderAlgorithm.models.Log;
+import com.recommender.recommenderAlgorithm.models.Serial;
 import com.recommender.recommenderAlgorithm.repositories.LogRepository;
 import com.recommender.recommenderAlgorithm.services.LogService;
 import com.recommender.recommenderAlgorithm.services.SerialService;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.*;
 
-@RestController
+@Controller
 @RequestMapping(path = "api/nonPersonal")
 public class NonPersonalRecommendations {
     private final LogService logService;
@@ -36,21 +40,26 @@ public class NonPersonalRecommendations {
             }
         }
     }
-    public List<String> formatMap() {
-        List<String> result = new LinkedList<String>();
+    public List<Serial> formatMap() {
+        List<Serial> result = new LinkedList<Serial>();
         Map<Integer, Integer> sortedMap = new TreeMap<Integer, Integer>(
                 Comparator.comparing(mapViews::get, (s1, s2) -> {
                     return s2.compareTo(s1);}));
         sortedMap.putAll(mapViews);
         Set<Integer> keys = sortedMap.keySet();
+        int i = 0;
         for (Integer key:keys) {
-            result.add(serialService.getById(key.longValue()).getSerialName());
+            result.add(serialService.getById(key.longValue()));
+            ++i;
+            if(i == 3)
+                return result;
         }
         return result;
     }
     @GetMapping
-    public List<String> getMostWatched(){
+    public String getMostWatched(Model model){
         calculateWatches();
-        return formatMap();
+        model.addAttribute("topSerials", formatMap());
+        return "topSerialsPage";
     }
 }
