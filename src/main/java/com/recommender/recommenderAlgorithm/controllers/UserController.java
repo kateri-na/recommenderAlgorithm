0a2f448a2389710e57a87ceab8df2489878f97c6
ЕкатеринаПанина <katerina_panina1@mail.ru>
2025-05-22 17:39:16 +0300

    add personal account page

--- src/main/java/com/recommender/recommenderAlgorithm/controllers/UserController.java
+++ src/main/java/com/recommender/recommenderAlgorithm/controllers/UserController.java
@@ -1,15 +1,19 @@
 package com.recommender.recommenderAlgorithm.controllers;
 
 import com.recommender.recommenderAlgorithm.models.User;
+import com.recommender.recommenderAlgorithm.security.UserDetails;
 import com.recommender.recommenderAlgorithm.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "api/users")
 public class UserController {
     private final UserService userService;
@@ -20,5 +24,13 @@ public class UserController {
     @GetMapping
     public List<User> getUsers(){
         return userService.getUsers();
+    }
+
+    @GetMapping("/personalAccount")
+    public String getUser(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("currentUser", userDetails.getUser());
+        return "personalAccount";
     }
 }
